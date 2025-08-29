0a19649364dce3ed1ee437c47076f0d4cd6e1264
WZC <15227663216@163.com>
2025-08-29 14:52:41 +0800

    simplify Func

--- LedgerFunc.c
+++ LedgerFunc.c
@@ -7,13 +7,14 @@ void init(struct database *lgPtr, FILE *fp)
         // 文件存在，读取数据
         lgPtr->fp = fp;
         fseek(fp, 0, SEEK_SET);                                                              // 将文件指针 fp 移动到文件的开头（偏移量为 0）
-        while (fread(&lgPtr->entries[lgPtr->count], sizeof(struct data), 1, lgPtr->fp) == 1) // 匹配fread数据块
+        while (fread(&lgPtr->entries[lgPtr->count], sizeof(struct data), 1, lgPtr->fp) == 1) // 匹配fwrite数据块
         {
             lgPtr->count++;
         }
     }
     else
     {
+        // 文件不存在，创建新文件
         printf("%s 文件不存在，是否创建该文件 y/n？\n", DATABASE_FILE_NAME);
         char ch = ' ';
         while ((ch = getchar()) != 'y')
@@ -24,47 +25,61 @@ void init(struct database *lgPtr, FILE *fp)
                 exit(EXIT_FAILURE);
             }
         }
-        fp = fopen(DATABASE_FILE_NAME, "wb"); // w选项会覆盖并创建文件
+        fp = fopen(DATABASE_FILE_NAME, "wb"); // w选项会覆盖与创建文件
+        *lgPtr = (struct database){0};
         lgPtr->fp = fp;
     }
 }
 
-void fullfill_entry(struct database *lgPtr)
+
+void add_entry(struct database *lgPtr)
 {
-    // fullfill id
-    snprintf(lgPtr->entries[lgPtr->count].id, 5, "%04d", lgPtr->count); // 格式化为 4 位数字，前面补零
-    // fullfill date
-    time_t t = time(NULL);
-    struct tm *tm = localtime(&t);
-    strftime(lgPtr->entries[lgPtr->count].date, 9, "%Y%m%d", tm); // 格式化为 "YYYYMMDD"
-    // fullfill thing,money,unit with str
-    int matched = sscanf(lgPtr->entries[lgPtr->count].str, "%[^0-9]%f%[^0-9]",
-                         lgPtr->entries[lgPtr->count].thing,
-                         &lgPtr->entries[lgPtr->count].money,
-                         lgPtr->entries[lgPtr->count].unit);
-    if (matched != 3)
-    {
-        printf("输入格式错误，无法解析事由、金额或单位。\n");
-        lgPtr->entries[lgPtr->count].thing[0] = '\0';
-        lgPtr->entries[lgPtr->count].money = 0.0;
-        lgPtr->entries[lgPtr->count].unit[0] = '\0';
-    }
+    write_entry(lgPtr, lgPtr->count);
     // increment count
     lgPtr->count++;
 }
 
-void write_entry(struct database *lgPtr)
+void delete_last_entry(struct database *lgPtr)
 {
-    printf("请输入条目：（如：买蔬菜花费20元）");
-    scanf("%s", lgPtr->entries[lgPtr->count].str);
+    if (lgPtr->count > 0)
+    {
+        char ch = ' ';
+        printf("确认删除最后一条记录吗？(y/n)：");
+        while ((ch = getchar()) == EOF && (ch = getchar()) == '\n')
+            ;
+        if ((ch = getchar()) == 'y')
+        {
+            lgPtr->entries[lgPtr->count - 1] = (struct data){0};
+            lgPtr->count--;
+        }
+    }
+    else
+    {
+        printf("没有记录可删除。\n");
+    }
+}
+
+void subtitute_existing_entry(struct database *lgPtr)
+{
+    printf("请输入要替换的条目编号（如：0001）：");
+    char id[5];
+    scanf("%4s", id);
+    if (atoi(id) >= lgPtr->count)
+    {
+        printf("编号不存在，无法替换。\n");
+    }
+    else
+    {
+        write_entry(lgPtr, atoi(id));
+    }
 }
 
 void print_database(struct database *lgPtr)
 {
     for (int i = 0; i < lgPtr->count; i++)
     {
-        printf("编号：%s\n日期：%s\n事由：%s\n内容：%s\n金额：%.2f\n单位：%s\n",
-               lgPtr->entries[i].id,
+        printf("编号：%d\n日期：%s\n事由：%s\n内容：%s\n金额：%.2f\n单位：%s\n",
+               atoi(lgPtr->entries[i].id)+1, // 显示从1开始的编号
                lgPtr->entries[i].date,
                lgPtr->entries[i].thing,
                lgPtr->entries[i].str,
@@ -89,4 +104,13 @@ void restore_database(struct database *lgPtr)
     {
         printf("文件未打开，无法保存数据。\n");
     }
+}
+
+void associate_database(struct database *lgPtr)
+{
+    printf("请输入要关联的账本文件名（如：.Ledger202509.dat）：");
+    char filename[100];
+    scanf("%s", filename);
+    FILE *new_fp = fopen(filename, "rb");
+    init(lgPtr, new_fp);
 }
--- LedgerFunc.h
+++ LedgerFunc.h
@@ -4,12 +4,16 @@
 #include <time.h>
 #include <stdlib.h> //for exit
 #include "LedgerStruc.h"
+#include "utils.h"
+
 
 #define DATABASE_FILE_NAME ".Ledger202509.dat"
 
-void fullfill_entry(struct database *);
-void write_entry(struct database *);
-void print_database(struct database *);
 void init(struct database *, FILE *);
+void add_entry(struct database *);
+void subtitute_existing_entry(struct database *);
+void delete_last_entry(struct database *);
+void print_database(struct database *);
 void restore_database(struct database *);
+void associate_database(struct database *);
 char UI_choose(struct database *);
